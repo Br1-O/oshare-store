866e2006ed483baef5f43c98767208713b3983b4
Br1-O <bruno.ortuno2@gmail.com>
2024-06-04 18:17:49 -0300

    added toast notifications via CDN for register, recover and contact forms

--- assets/js/validation/contactForm.js
+++ assets/js/validation/contactForm.js
@@ -19,69 +19,106 @@ export const validationContactForm = (maxLengthValidation, minLengthValidation, 
     //validation for login fields
     formContact.addEventListener("submit", (event) => {
   
-        //prevent submit of form
-        event.preventDefault();
-  
-        //empty all previous error messages
-        for (const error of errorMessages) {
-        error.classList.add("d-none");
-        error.innerText = "";
-        }
-  
-        //check validations for fields
-        let isEmailValid = emailValidation(emailField, emailErrorMessage);
-        let isPhoneValid = false;
-        let isSubjectValid = false;
-        let isCommentValid = false;
-  
-        if (isEmailValid) {
-            isPhoneValid = phoneNumberValidation(phoneField, phoneErrorMessage, false);
-        }
-
-        if (isEmailValid && isPhoneValid) {
-            isSubjectValid = minLengthValidation(subjectField, subjectErrorMessage, 5, "*el asunto es muy corto");
-        
-            if (isSubjectValid) {
-                isSubjectValid = maxLengthValidation(subjectField, subjectErrorMessage, 20, "*20 caracteres máximo");
-            }
-        }
-
-        if (isEmailValid && isPhoneValid && isSubjectValid) {
-            isCommentValid = maxLengthValidation(commentField, commentErrorMessage, 500, "*500 caracteres máximo");
-        
-            if (isCommentValid) {
-                isCommentValid = minLengthValidation(commentField, commentErrorMessage, 5, "*el mensaje es muy corto");
-            }
-        }
-  
-        //submit contact form if all fields are valid
-        if(isEmailValid && isPhoneValid && isSubjectValid && isCommentValid){
-  
-            //input data
-            let data = {
-                email : emailField.value,
-                phone : phoneField.value,
-                subject : subjectField.value,
-                comment : commentField.value
+        try {
+             
+            //prevent submit of form
+            event.preventDefault();
+    
+            //empty all previous error messages
+            for (const error of errorMessages) {
+            error.classList.add("d-none");
+            error.innerText = "";
             }
     
-            //submit contact form
-            let submitForm = () => {
-
-                //dummy function for submit
-                console.log(data);
-
-                //success message
-                alert("¡Formulario enviado con exito! Nuestros representantes lo contactaran a la brevedad.");
-
-                //reset form fields values
-                formContact.reset();
-
-                //set focus on first field
-                emailField.focus()
+            //check validations for fields
+            let isEmailValid = emailValidation(emailField, emailErrorMessage);
+            let isPhoneValid = false;
+            let isSubjectValid = false;
+            let isCommentValid = false;
+    
+            if (isEmailValid) {
+                isPhoneValid = phoneNumberValidation(phoneField, phoneErrorMessage, false);
             }
-  
-            submitForm();
+
+            if (isEmailValid && isPhoneValid) {
+                isSubjectValid = minLengthValidation(subjectField, subjectErrorMessage, 5, "*el asunto es muy corto");
+            
+                if (isSubjectValid) {
+                    isSubjectValid = maxLengthValidation(subjectField, subjectErrorMessage, 20, "*20 caracteres máximo");
+                }
+            }
+
+            if (isEmailValid && isPhoneValid && isSubjectValid) {
+                isCommentValid = maxLengthValidation(commentField, commentErrorMessage, 500, "*500 caracteres máximo");
+            
+                if (isCommentValid) {
+                    isCommentValid = minLengthValidation(commentField, commentErrorMessage, 5, "*el mensaje es muy corto");
+                }
+            }
+    
+            //submit contact form if all fields are valid
+            if(isEmailValid && isPhoneValid && isSubjectValid && isCommentValid){
+    
+                //input data
+                let data = {
+                    email : emailField.value,
+                    phone : phoneField.value,
+                    subject : subjectField.value,
+                    comment : commentField.value
+                }
+        
+                //submit contact form
+                let submitForm = () => {
+
+                    //dummy function for submit
+                    console.log(data);
+
+                    //Toast notification for success
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: "bottom-end",
+                        showConfirmButton: false,
+                        timer: 3800,
+                        timerProgressBar: false,
+                        didOpen: (toast) => {
+                        toast.onmouseenter = Swal.stopTimer;
+                        toast.onmouseleave = Swal.resumeTimer;
+                        }
+                    });
+                    Toast.fire({
+                        icon: "success",
+                        title: `¡Formulario enviado con exito! <br> Nuestros representantes lo contactaran a la brevedad.`
+                    });
+
+                    //reset form fields values
+                    formContact.reset();
+
+                    //set focus on first field
+                    emailField.focus()
+                }
+    
+                submitForm();
+            }
+
+        } catch (error) {
+
+            //Toast notification for failure
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "bottom-end",
+                showConfirmButton: false,
+                timer: 3800,
+                timerProgressBar: false,
+                didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+                }
+            });
+            Toast.fire({
+                icon: "error",
+                title: `¡No se pudo enviar el formulario en este momento! :( <br><br> Por favor, intente otra de nuestras vías de comunicación.`
+            });
         }
+
     });
 }
--- assets/js/validation/login&register.js
+++ assets/js/validation/login&register.js
@@ -1,4 +1,3 @@
-import { fetchData } from "../utils/fetch.js";
 import { accountTemplate, accountModal } from "../modals/account.js";
 import { setSessionData, setUserDataFromSessionData } from "../utils/sessionStorage.js";
 import { redirectToPage } from "../utils/redirectToPage.js";
@@ -47,265 +46,365 @@ export const validationLoginRegister = () => {
   //validation for login fields and implementation for login
   formLogin.addEventListener("submit", (event) => {
 
-    //prevent submit of form
-    event.preventDefault();
+    try {
+      
+      //prevent submit of form
+      event.preventDefault();
 
-    //empty all previous error messages
-    for (const error of errorMessages) {
-      error.classList.add("d-none");
-      error.innerText = "";
-    }
-
-    let isEmailValid = emailValidation(emailLogin, emailLoginErrorMessage);
-    let isPasswordValid = false;
-
-    if (isEmailValid) {
-      isPasswordValid = passwordValidation(passwordLogin, passwordLoginErrorMessage);
-    }
-
-    //submit login petition if all fields are valid
-    if(isEmailValid && isPasswordValid){
-
-      //input data
-      let email = emailLogin.value;
-      let hashedPassword = CryptoJS.SHA256(passwordLogin.value).toString(CryptoJS.enc.Hex);
-
-      //verification to dummy data (later will be switched for fetch function to backend db)
-      let submitLogin = async () => {
-        let validUser = false;
-
-        //check if oshare_designs object already exists in localstorage
-        let oshareLocal = searchInLocalStorage("oshare_designs");
-
-        //if it exist
-        if (oshareLocal) {
-
-          //parse the JSON into a Js object
-          oshareLocal = JSON.parse(oshareLocal);
-
-          //iterate through users in the object
-          oshareLocal.users.forEach(user => {
-            //check if email exists
-            if (user.email == email) {
-
-              validUser = true;
-
-              //check if passwords match
-              if (user.key === hashedPassword) {
-  
-                //set session object
-                let sessionData = {
-                  "name" : user.name,
-                  "surname" : user.surname,
-                  "email" : user.email,
-                  "picture" : user.profileImage,
-                  "cart" : user.cart,
-                  "isSessionSet" : true
-                }
-  
-                //set session data
-                setSessionData("oshare_designs_session", JSON.stringify(sessionData));
-
-                //set user object
-                setUserDataFromSessionData(userData);
-  
-                //reset form fields values
-                formRegister.reset();
-                formLogin.reset();
-                formRecover.reset();
-                    
-                //close modal window
-                closeModal();
-  
-                //redirect to logged page
-                redirectToPage((window.location.hash).slice(1), 0);
-  
-                //update content based on change of session state
-                updateContent();
-  
-              } else {
-                //display error message
-                emailLoginErrorMessage.classList.remove("d-none");
-                emailLoginErrorMessage.innerText = "¡Usuario o contraseña incorrectos!";
-  
-                emailLogin.focus();
-              }
-            }
-          });
-        }
-
-        if (!validUser) {
-          //display error message
-          emailLoginErrorMessage.classList.remove("d-none");
-          emailLoginErrorMessage.innerText = "¡Usuario o contraseña incorrectos!";
-
-          emailLogin.focus();
-        }
+      //empty all previous error messages
+      for (const error of errorMessages) {
+        error.classList.add("d-none");
+        error.innerText = "";
       }
 
-      submitLogin();
+      let isEmailValid = emailValidation(emailLogin, emailLoginErrorMessage);
+      let isPasswordValid = false;
+
+      if (isEmailValid) {
+        isPasswordValid = passwordValidation(passwordLogin, passwordLoginErrorMessage);
+      }
+
+      //submit login petition if all fields are valid
+      if(isEmailValid && isPasswordValid){
+
+        //input data
+        let email = emailLogin.value;
+        let hashedPassword = CryptoJS.SHA256(passwordLogin.value).toString(CryptoJS.enc.Hex);
+
+        //verification to dummy data (later will be switched for fetch function to backend db)
+        let submitLogin = async () => {
+          let validUser = false;
+
+          //check if oshare_designs object already exists in localstorage
+          let oshareLocal = searchInLocalStorage("oshare_designs");
+
+          //if it exist
+          if (oshareLocal) {
+
+            //parse the JSON into a Js object
+            oshareLocal = JSON.parse(oshareLocal);
+
+            //iterate through users in the object
+            oshareLocal.users.forEach(user => {
+              //check if email exists
+              if (user.email == email) {
+
+                validUser = true;
+
+                //check if passwords match
+                if (user.key === hashedPassword) {
+    
+                  //set session object
+                  let sessionData = {
+                    "name" : user.name,
+                    "surname" : user.surname,
+                    "email" : user.email,
+                    "picture" : user.profileImage,
+                    "cart" : user.cart,
+                    "isSessionSet" : true
+                  }
+    
+                  //set session data
+                  setSessionData("oshare_designs_session", JSON.stringify(sessionData));
+
+                  //set user object
+                  setUserDataFromSessionData(userData);
+    
+                  //reset form fields values
+                  formRegister.reset();
+                  formLogin.reset();
+                  formRecover.reset();
+                      
+                  //close modal window
+                  closeModal();
+    
+                  //redirect to logged page
+                  redirectToPage((window.location.hash).slice(1), 0);
+    
+                  //update content based on change of session state
+                  updateContent();
+    
+                } else {
+                  //display error message
+                  emailLoginErrorMessage.classList.remove("d-none");
+                  emailLoginErrorMessage.innerText = "¡Usuario o contraseña incorrectos!";
+    
+                  emailLogin.focus();
+                }
+              }
+            });
+          }
+
+          if (!validUser) {
+            //display error message
+            emailLoginErrorMessage.classList.remove("d-none");
+            emailLoginErrorMessage.innerText = "¡Usuario o contraseña incorrectos!";
+
+            emailLogin.focus();
+          }
+        }
+
+        submitLogin();
+      }
+
+    } catch (error) {
+
+      //Toast notification for failure
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "bottom-end",
+        showConfirmButton: false,
+        timer: 3500,
+        timerProgressBar: false,
+        didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+        }
+      });
+      Toast.fire({
+          icon: "error",
+          title: `¡No se pudo iniciar sesión! :( <br><br> Por favor, intentelo más tarde.`
+      });
+
     }
+
   });
   
   //validation for register fields and implementation for register
   formRegister.addEventListener("submit", (event) => {
         
-    //prevent submit of form
-    event.preventDefault();
+    try {
+      
+      //prevent submit of form
+      event.preventDefault();
 
-    //empty all previous error messages
-    for (const error of errorMessages) {
-      error.classList.add("d-none");
-      error.innerText = "";
-    }
-
-    let isEmailValid = emailValidation(emailRegister, EmailRegisterErrorMessage);
-    let areEmailsEqual = false;
-    let isPasswordValid = false;
-    let arePasswordsEqual = false;
-
-    if (isEmailValid) {
-      areEmailsEqual = areValuesEqual(emailRegister, emailConfirmRegister, EmailConfirmRegisterErrorMessage, "*los correos deben coincidir");
-    }
-
-    if (isEmailValid && areEmailsEqual) {
-      isPasswordValid = passwordValidation(passwordRegister, PasswordRegisterErrorMessage);
-    }
-
-    if (isEmailValid && areEmailsEqual && isPasswordValid) {
-      arePasswordsEqual = areValuesEqual(passwordRegister, passwordConfirmRegister, PasswordConfirmRegisterErrorMessage, "*las contraseñas deben coincidir");
-    }
-
-    //submit register petition if all fields are valid
-    if(isEmailValid && areEmailsEqual && isPasswordValid && arePasswordsEqual){
-      //input data
-      let email = emailRegister.value;
-      let hashedPassword = CryptoJS.SHA256(passwordRegister.value).toString(CryptoJS.enc.Hex);
-
-      let validUser = true;
-
-      //verification to dummy data (later will be switched for fetch function to backend db)
-      let submitRegister = async () => {
-
-        //check if oshare_designs object is already in localstorage
-        let oshareLocal = searchInLocalStorage("oshare_designs");
-
-        //If not create empty object
-        if (!oshareLocal) {
-
-          let oshareLocalObject = 
-          {
-            users: [],
-            preferences: []
-          }
-
-          //set empty object into localstorage
-          setLocalData("oshare_designs", JSON.stringify(oshareLocalObject));
-
-          //get empty object from localstorage
-          oshareLocal = getLocalData("oshare_designs");
-        }
-
-        //parse the JSON into Js object
-        oshareLocal = JSON.parse(oshareLocal);
-
-        //iterate through users store in the object
-        oshareLocal.users.forEach(user => {
-          //check if email is already registered
-          if (user.email == emailRegister.value) {
-            validUser = false;
-          }
-        });
-
-        //if register input is valid
-        if (validUser) {
-
-          //display message
-          alert("¡Registro exitoso con el correo: " + email + " !");
-
-          //reset form fields values
-          formRegister.reset();
-          formLogin.reset();
-          formRecover.reset();
-
-          //close modal window
-          closeModal();
-
-          //set user object for localStorage
-          let user = {
-            "name" : "",
-            "surname" : "",
-            "email" : email,
-            "key" : hashedPassword,
-            "profileImage" : "",
-            "cart" : []
-          }
-
-          //add user to oshareLocal's users
-          oshareLocal.users.push(user);
-          //set localStorage data
-          setLocalData("oshare_designs", JSON.stringify(oshareLocal));
-
-          //set data to user object
-          userData.email = email;
-
-          //change session state
-          userData.isSessionSet = true;
-
-          //set session object for sessionStorage
-          let sessionData = {
-            "name" : "",
-            "surname" : "",
-            "email" : email,
-            "picture" : "",
-            "cart" : [],
-            "isSessionSet" : true
-          }
-
-          //set session data
-          setSessionData("oshare_designs_session", JSON.stringify(sessionData));
-
-          //redirect to logged page
-          redirectToPage((window.location.hash).slice(1), 0);
-
-          //update content based on change of session state
-          updateContent();
-
-        }else{
-          //display message
-          EmailRegisterErrorMessage.classList.remove("d-none");
-          EmailRegisterErrorMessage.innerText= "¡Este correo ya está registrado!";
-
-          emailRegister.focus();
-        }
+      //empty all previous error messages
+      for (const error of errorMessages) {
+        error.classList.add("d-none");
+        error.innerText = "";
       }
 
-      submitRegister();
+      let isEmailValid = emailValidation(emailRegister, EmailRegisterErrorMessage);
+      let areEmailsEqual = false;
+      let isPasswordValid = false;
+      let arePasswordsEqual = false;
+
+      if (isEmailValid) {
+        areEmailsEqual = areValuesEqual(emailRegister, emailConfirmRegister, EmailConfirmRegisterErrorMessage, "*los correos deben coincidir");
+      }
+
+      if (isEmailValid && areEmailsEqual) {
+        isPasswordValid = passwordValidation(passwordRegister, PasswordRegisterErrorMessage);
+      }
+
+      if (isEmailValid && areEmailsEqual && isPasswordValid) {
+        arePasswordsEqual = areValuesEqual(passwordRegister, passwordConfirmRegister, PasswordConfirmRegisterErrorMessage, "*las contraseñas deben coincidir");
+      }
+
+      //submit register petition if all fields are valid
+      if(isEmailValid && areEmailsEqual && isPasswordValid && arePasswordsEqual){
+        //input data
+        let email = emailRegister.value;
+        let hashedPassword = CryptoJS.SHA256(passwordRegister.value).toString(CryptoJS.enc.Hex);
+
+        let validUser = true;
+
+        //verification to dummy data (later will be switched for fetch function to backend db)
+        let submitRegister = async () => {
+
+          //check if oshare_designs object is already in localstorage
+          let oshareLocal = searchInLocalStorage("oshare_designs");
+
+          //If not create empty object
+          if (!oshareLocal) {
+
+            let oshareLocalObject = 
+            {
+              users: [],
+              preferences: []
+            }
+
+            //set empty object into localstorage
+            setLocalData("oshare_designs", JSON.stringify(oshareLocalObject));
+
+            //get empty object from localstorage
+            oshareLocal = getLocalData("oshare_designs");
+          }
+
+          //parse the JSON into Js object
+          oshareLocal = JSON.parse(oshareLocal);
+
+          //iterate through users store in the object
+          oshareLocal.users.forEach(user => {
+            //check if email is already registered
+            if (user.email == emailRegister.value) {
+              validUser = false;
+            }
+          });
+
+          //if register input is valid
+          if (validUser) {
+
+            //Toast notification for success
+            const Toast = Swal.mixin({
+              toast: true,
+              position: "bottom-end",
+              showConfirmButton: false,
+              timer: 3000,
+              timerProgressBar: false,
+              didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+              }
+            });
+            Toast.fire({
+              icon: "success",
+              title: `¡Registro exitoso con el correo: <br> ${email} ! `
+            });
+
+            //reset form fields values
+            formRegister.reset();
+            formLogin.reset();
+            formRecover.reset();
+
+            //close modal window
+            closeModal();
+
+            //set user object for localStorage
+            let user = {
+              "name" : "",
+              "surname" : "",
+              "email" : email,
+              "key" : hashedPassword,
+              "profileImage" : "",
+              "cart" : []
+            }
+
+            //add user to oshareLocal's users
+            oshareLocal.users.push(user);
+            //set localStorage data
+            setLocalData("oshare_designs", JSON.stringify(oshareLocal));
+
+            //set data to user object
+            userData.email = email;
+
+            //change session state
+            userData.isSessionSet = true;
+
+            //set session object for sessionStorage
+            let sessionData = {
+              "name" : "",
+              "surname" : "",
+              "email" : email,
+              "picture" : "",
+              "cart" : [],
+              "isSessionSet" : true
+            }
+
+            //set session data
+            setSessionData("oshare_designs_session", JSON.stringify(sessionData));
+
+            //redirect to logged page
+            redirectToPage((window.location.hash).slice(1), 0);
+
+            //update content based on change of session state
+            updateContent();
+
+          }else{
+            //display message
+            EmailRegisterErrorMessage.classList.remove("d-none");
+            EmailRegisterErrorMessage.innerText= "¡Este correo ya está registrado!";
+
+            emailRegister.focus();
+          }
+        }
+
+        submitRegister();
+      }
+
+    } catch (error) {
+
+      //Toast notification for failure
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "bottom-end",
+        showConfirmButton: false,
+        timer: 3500,
+        timerProgressBar: false,
+        didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+        }
+      });
+      Toast.fire({
+          icon: "error",
+          title: `¡No se pudo registrar el usuario! <br><br> Por favor, intentelo más tarde.`
+      });
+
     }
+
   });
 
   //validation for recover password field and implementation for recover
   formRecover.addEventListener("submit", (event) => {
 
-    //prevent submit of form
-    event.preventDefault();
+    try {
+      
+      //prevent submit of form
+      event.preventDefault();
 
-    //empty all previous error messages
-    for (const error of errorMessages) {
-      error.classList.add("d-none");
-      error.innerText = "";
-    }
+      //empty all previous error messages
+      for (const error of errorMessages) {
+        error.classList.add("d-none");
+        error.innerText = "";
+      }
 
-    //check email
+      //check email
       if(emailValidation(emailRecover, emailRecoverErrorMessage)){
-        //display message
-        alert("¡Chequee su correo y siga las instrucciones!");
+        //Toast notification for success
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "bottom-end",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: false,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+          }
+        });
+        Toast.fire({
+          icon: "info",
+          title: `Correo enviado a: ${emailRecover.value} <br><br> ¡Siga las instrucciones para recuperar su cuenta!`
+        });
+
         //reset register fields value
         formRecover.reset();
         //switch to login modal
         accountModal(accountTemplate);
       }
+
+    } catch (error) {
+      //Toast notification for failure
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "bottom-end",
+        showConfirmButton: false,
+        timer: 3500,
+        timerProgressBar: false,
+        didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+        }
+      });
+      Toast.fire({
+          icon: "error",
+          title: `¡No se pudo enviar el mensaje de recuperación! <br><br> Por favor, intentelo más tarde.`
+      });
+    }
+
   });
+
 }
 
 
